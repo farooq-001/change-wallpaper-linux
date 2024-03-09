db7962f5f6c7ba3305bc39cc2437ef1785f69469
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-03-10 01:01:46 +0530

    Create change-wallpaper-linux-2.0.sh

---
+++ change-wallpaper-linux-2.0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo tar -zxvf change-wallpaper-linux.tar.gz
+sudo chmod +x change-wallpaper-linux/*
+sudo mv  change-wallpaper-linux /home
+sudo mv  change-wallpaper-linux/change-wallpaper.service   /etc/systemd/system/change-wallpaper.service
+sudo tee << .bashrc EOF
+/home/change-wallpaper-linux/change-wallpaper.sh
+EOF
+
+
+
+
