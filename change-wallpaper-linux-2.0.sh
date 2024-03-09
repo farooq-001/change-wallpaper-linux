61d2a6f5c59b0955e8b827d93a81e76c8a6b2017
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-03-10 01:11:26 +0530

    Update change-wallpaper-linux-2.0.sh

--- change-wallpaper-linux-2.0.sh
+++ change-wallpaper-linux-2.0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# set -e
 
 # Update package lists
 sudo apt update
@@ -9,15 +10,22 @@ sudo apt install python3 python3-pip -y
 # Install schedule package
 sudo pip install schedule
 
+# Extract and configure change-wallpaper-linux
 sudo tar -zxvf change-wallpaper-linux.tar.gz
 sudo chmod +x change-wallpaper-linux/*
-sudo mv  change-wallpaper-linux /home
-sudo mv  change-wallpaper-linux/change-wallpaper.service   /etc/systemd/system/change-wallpaper.service
-sudo tee << .bashrc EOF
+sudo mv change-wallpaper-linux /home
+sudo mv change-wallpaper-linux/change-wallpaper.service /etc/systemd/system/change-wallpaper.service
+
+# Add script to .bashrc for automatic execution
+sudo tee -a ~/.bashrc <<EOF
 /home/change-wallpaper-linux/change-wallpaper.sh
 EOF
 
-source .bahsrc
+# Reload .bashrc
+source ~/.bashrc
+
+# Enable and start the service
 sudo systemctl enable change-wallpaper.service
 sudo systemctl start change-wallpaper.service
 
+echo "Installation and setup completed successfully."
