e84289e4e2854ee4154e549ef88704bf60e48484
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-03-10 01:02:57 +0530

    Update change-wallpaper-linux-2.0.sh

--- change-wallpaper-linux-2.0.sh
+++ change-wallpaper-linux-2.0.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Update package lists
+sudo apt update
+
+# Install Python 3 and pip
+sudo apt install python3 python3-pip -y
+
+# Install schedule package
+sudo pip install schedule
+
 sudo tar -zxvf change-wallpaper-linux.tar.gz
 sudo chmod +x change-wallpaper-linux/*
 sudo mv  change-wallpaper-linux /home
