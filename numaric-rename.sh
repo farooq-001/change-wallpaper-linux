c45898a1c5800833cbe6c7cfd91691e46df7d0b8
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-03-23 22:07:21 +0530

    Update numaric-rename.sh

--- numaric-rename.sh
+++ numaric-rename.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# Read source directory path from user input
+read -rp "Enter source directory path: " source_dir
+
+# Check if the specified directory exists
+if [ ! -d "$source_dir" ]; then
+    echo "Directory $source_dir does not exist."
+    exit 1
+fi
+
 # Go to the specified directory
-cd /home/farooq/Desktop/Wlp/ || exit
+cd "$source_dir" || exit
 
-# Counter for renaming
+# Rename files with numeric names from 1 to 100
 count=1
-
-# Loop through each file in the directory
 for file in *; do
-    # Check if the file is a regular file (not a directory)
-    if [ -f "$file" ]; then
-        # Rename the file with a numeric name
-        mv "$file" "${count}.jpg"  # Assuming the files are JPEG images, change the extension if needed
-        count=$((count + 1))
-    fi
+    mv "$file" "${count}.jpg" && ((count++))
+    [ $count -gt 100 ] && break
 done
-
