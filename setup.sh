494655556cd3ec44ec5bae8f5864dca9bf1abbf6
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-03-30 15:18:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@ setup() {
     sudo mkdir /home/change-wallpaper-linux
 
     # Unzip files
-    #sudo unzip Wlp-2.zip
     sudo unzip Wlp.zip
+    #sudo unzip Wlp-2.zip
+    
 
     # Create subdirectory and copy files
-    sudo mkdir /home/change-wallpaper-linux/Wlp
-    sudo cp -r Wlp/* /home/change-wallpaper-linux/Wlp/
-    sudo cp -r Wlp-2/* /home/change-wallpaper-linux/Wlp/
+    sudo mv Wlp /home/change-wallpaper-linux/Wlp/
+    sudo mv Wlp-2/* /home/change-wallpaper-linux/Wlp/
 
     # Copy python script and systemd service file
-    sudo cp -r change-wallpaper.py /home/change-wallpaper-linux/
+    sudo cp -r change-wallpaper.py   /etc/
     sudo cp -r change-wallpaper.service /etc/systemd/system/change-wallpaper.service
 
     # Enable and start the systemd service
